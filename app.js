7d0bad275666becee96236c2c3716a92d59c5ad2
Yang, Alex <alex.yang@cbsinteractive.com>
2014-07-17 14:14:58 -0700

    get basic search functionality working, stream to audio tag

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.bodyParser());
 //routes
 app.get('/', index.view);
 //set environment ports and start application
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 2014);
 http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express server listening on port ' + app.get('port'));
 });
---
+++ public/js/controllers.js
@@ -0,0 +1,36 @@
+var spotifyApp = angular.module('spotifyApp', []);
+spotifyApp.config(function($interpolateProvider) { $interpolateProvider.startSymbol('(('); $interpolateProvider.endSymbol('))'); });
+
+spotifyApp.controller('SongListCtrl', function ($scope, $http) {
+  //populate songs from ajax call, which gets triggered no whenever they search
+  //need to use spotify api to grab from existing list of songs?
+  //or just grab the json with an ajax call
+
+  $scope.query = "chu";
+
+  $scope.searchSongs = function(){
+    $http({method: 'GET', url: 'http://ws.spotify.com/search/1/track.json?q=' + $scope.query}).
+    success(function(data, status, headers, config) {
+      // this callback will be called asynchronously
+      // when the response is available
+      $scope.songs = data.tracks;
+      $scope.artists = data.tracks.artists;
+      // console.log("tracks: " + data.tracks);
+      // console.log("track href: " + data.tracks[0].href);
+      // console.log("artists: " + data.tracks.artists);
+    }).
+    error(function(data, status, headers, config) {
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+      console.log("error retrieving data from Spotify");
+    });
+  }
+
+  $scope.selectSong = function(trackId){
+    console.log("track ID recieved: " + trackId);
+    
+  }
+
+
+
+});
